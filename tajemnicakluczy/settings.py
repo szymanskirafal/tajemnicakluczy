cf21668146311a40216e2c7fab9fc4168fbc983f
RafalSzymanski <r.szymansky@gmail.com>
2020-01-21 21:59:04 +0000

    Set secrets

--- tajemnicakluczy/settings.py
+++ tajemnicakluczy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8+_**b!p1@aoaz4!^8k4mf0(s8ipmj-o7+@qd2x(6%qejc^^n5'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
